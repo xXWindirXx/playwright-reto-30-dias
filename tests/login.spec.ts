9c9615f11446f2b8ae5d4da572e99afd32e2e133
Omar Garcia <omareduardogj@gmail.com>
2026-08-03 16:35:33 -0600

    Reto 1 Completado

---
+++ tests/login.spec.ts
@@ -0,0 +1,11 @@
+import {expect, test} from '@playwright/test';
+
+test ('Login to hrm', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password'}).fill('admin123');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible();
+    
+})
